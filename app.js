a4ce03ac524ccbff2b679ad065ccc08d7be1039c
Pushpa Mali <94977416+Pushpa-Mali@users.noreply.github.com>
2023-07-20 13:27:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+function handleCLick(elm){
+    console.log("hello");
+    let elmId = elm.id;
+    console.log(elmId);
+    // var divelm = document.getElementById("wrap");
+    // divelm.style.opacity="0.2";
+}
+
